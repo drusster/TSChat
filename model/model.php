600c28a044bd1ac62e6023c9ba77e0b5207aaa60
drusster <druss@fara70.ru>
2018-04-14 17:22:16 +0700

    этот пустой

--- model/model.php
+++ model/model.php
@@ -1,8 +1,23 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class DB
+{
+    static $link_db;
+    static $count = 0; 
+
+    
+    public static function connect()
+    {// Синглтончик-с в целях экономии
+        if(empty(self::$link_db))
+        {
+            self::$link_db = @mysqli_connect(HOST, USER, PASS,DB) 
+                           or die('No connect'); 
+            
+            mysqli_set_charset(self::$link_db, 'utf8');
+        }
+    }
+}
+
+// Запускаем не отходя от кассы
+DB::connect();
 
