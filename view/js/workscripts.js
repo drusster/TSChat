f144a93648a96ee426e5f55c19ce8d0bec01a934
drusster <druss@fara70.ru>
2018-04-14 19:11:39 +0700

    Надоело ёрзать мышкой, задействовал клавишу enter в форме регистрации.

--- view/js/workscripts.js
+++ view/js/workscripts.js
@@ -2,13 +2,20 @@ jQuery(document).ready(function($){
 $( "#reg" ).click(function() {
     event.preventDefault();
     if($( "input[name=login]" ).val() === ""){
-        $( "input[name=login]" ).attr({"placeholder":"Лонин не может быть пустым!"});
+        $( "input[name=login]" ).attr({"placeholder":"Логин не может быть пустым!"});
     }else if( $( "input[name=pass]" ).val() === ""){
         $( "input[name=pass]" ).attr({"placeholder":"Пароль не может быть пустым!"});
     }else if ($(this).attr("name") === "registration"){ //добавлю скрытыю переменную перед отправкой регистрационной формы
         $("form[name=form_login_and_pass]").append("<input type='hidden' name='registration' />");
         $("form[name=form_login_and_pass]").submit();
     }  
+});
+
+
+$("form[name=form_login_and_pass]").keypress(function(e){
+    if(e.keyCode==13){
+        $("#reg").trigger("click");
+    }
 });
     
     
