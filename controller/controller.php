26ae47c0a013375ef7a735cdfc71c98486d61b7d
drusster <druss@fara70.ru>
2018-04-14 16:12:14 +0700

    начал делать форму отправки регистрации

--- controller/controller.php
+++ controller/controller.php
@@ -8,7 +8,15 @@ require_once 'functions/functions.php';
 // получение динамичной части шаблона .content
 $view = empty($_GET['view']) ? 'enter' : $_GET['view'];
 
+switch ($view) {
+    case 'reg':
+        $headeradd = " - регистрация";
 
+        break;
+
+    default:
+        break;
+}
 
 
 // подключени вида
--- view/enter.php
+++ view/enter.php
@@ -1,13 +1,11 @@
-<div class="main">
     <div class="btn-conteiner left">
         <a href="index.php?view=logon">
             Войти
         </a>
     </div>
     <div class="btn-conteiner right">
-        <a href="index.php?view=logout">
+        <a href="index.php?view=reg">
             Регистрация
         </a>
     </div>
-</div>
    
